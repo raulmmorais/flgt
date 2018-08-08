518a4f54eb98187bb96161a214d29c3994250b05
Raul Morais <raulmmorais@gmail.com>
2018-08-07 22:37:45 -0300

    Adicionado menu do jogo. Metodo render não está funcionando, preciso descobrir por que.

--- core/src/com/tutorial/game/views/LoadingScreen.java
+++ core/src/com/tutorial/game/views/LoadingScreen.java
@@ -2,6 +2,7 @@ package com.tutorial.game.views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.tutorial.game.Box2DTutorial;
 
 public class LoadingScreen implements Screen {
@@ -19,6 +20,8 @@ public class LoadingScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         Gdx.app.log("create", "call Menu");
         parent.changeScreen(Box2DTutorial.MENU);
     }
